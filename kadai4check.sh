5d8a20f690c176971e882b4ddc2e624c5d5ae8c0
oyamaAiit <a2008io@aiit.ac.jp>
2020-07-05 18:27:20 +0900

    kadai4check.sh update

--- kadai4check.sh
+++ kadai4check.sh
@@ -2,15 +2,15 @@
 
 #テスト内容
 
-./bash kadai4.sh 3
+./kadai4.sh 3
 #自然数の指定が一つだけなのでエラー
-./bash kadai4.sh test 50
+./kadai4.sh test 50
 #一つ目の自然数が文字なのでエラー
-./bash kadai4.sh 20 test
+./kadai4.sh 20 test
 #二つ目の自然数が文字なのでエラー
-./bash kadai4.sh 465 360
+./kadai4.sh 465 360
 #最大公約数15
-./bash kadai4.sh 0 5
+./kadai4.sh 0 5
 #最大公約数0
 ./kadai4.sh 49 63
 #最大公約数7
